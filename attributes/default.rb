ab3def9f577594eb0731053898515d6300f29387
Peter Halliday <pghalliday@gmail.com>
2015-12-03 23:09:13 +0100

    correct checksum for beats dashboards

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@
 default['formatron_monitor']['configuration'] = nil
 
 default['formatron_monitor']['beats_dashboards']['version'] = '1.0.0'
-default['formatron_monitor']['beats_dashboards']['checksum'] = '1.0.0'
+default['formatron_monitor']['beats_dashboards']['checksum'] = '63afec8a11a4df2bb87a9ef62dc3c96c43c845fa7a96a146afed894aa88b5bfd'
